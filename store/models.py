9a29cfa2c54bd27175996951ca92bcbd5e0abc39
majid <majid.asadollahi95@gmail.com>
2022-08-20 23:53:19 +0430

    create models in store app

--- store/models.py
+++ store/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField()
+
+    name = 
+    
+
+
+
--- storefront/settings.py
+++ storefront/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     
     # local apps
     'playground',
+    'store',
+    'tags',
 
 ]
 
