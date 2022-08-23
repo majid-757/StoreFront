66897197a112875b70ee4ba1a4e5660a0881748a
majid <majid.asadollahi95@gmail.com>
2022-08-23 23:13:09 +0430

    Add order table in customer model

--- store/models.py
+++ store/models.py
@@ -33,4 +33,17 @@ class Customer(models.Model):
 
 
 
+class Order(models.Model):
+    PAYMENT_STATUS_PENDING = 'p'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'F'
+    PAYMENT_STATUS_CHOICES = [
+        (PAYMENT_STATUS_PENDING, 'Pending'),
+        (PAYMENT_STATUS_COMPLETE, 'Complete'),
+        (PAYMENT_STATUS_FAILED, 'Failed'),
+    ]
+
+
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
 
