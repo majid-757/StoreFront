682af6e687fca8ac9d06e36d51592725a247f770
majid <majid.asadollahi95@gmail.com>
2022-08-24 22:48:14 +0430

    Add Address table in customer model

--- store/models.py
+++ store/models.py
@@ -47,3 +47,18 @@ class Order(models.Model):
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
 
+
+
+
+
+
+class Address(models.Model):
+    street = models.CharField(max_length=225)
+    city = models.CharField(max_length=255)
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
+
+
+
+
+
+
