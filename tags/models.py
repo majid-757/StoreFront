82bd1e5f49c8559ad9203153fcb994f5109fc90c
majid <majid.asadollahi95@gmail.com>
2022-09-02 00:05:52 +0430

    Add new model in Leked Item app

--- tags/models.py
+++ tags/models.py
@@ -27,7 +27,8 @@ class TaggedItem(models.Model):
 class LikedItem(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
 
 
 
